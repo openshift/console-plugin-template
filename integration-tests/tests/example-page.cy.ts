4ecb88d96092f8ac9ec7b39b64c64ca20d494c07
CYRIL AJIEH <cyril.ajieh@gmail.com>
2023-04-21 15:57:46 -0400

    fix login and logout issue in CI

--- integration-tests/tests/example-page.cy.ts
+++ integration-tests/tests/example-page.cy.ts
@@ -12,7 +12,7 @@ const installHelmChart = (path: string) => {
       failOnNonZeroExit: false,
     },
   ).then((result) => {
-    cy.visit(`/example`);
+    cy.visit(`/dashboards`);
     cy.log('Error installing helm chart: ', result.stderr);
     cy.log('Successfully installed helm chart: ', result.stdout);
   });
@@ -30,70 +30,51 @@ const deleteHelmChart = (path: string) => {
   });
 };
 
-// Adding this to further debug the failing test issues in CI
-describe('My First Test without installing hel chart', () => {
-  it('Does not do much! Should pass', () => {
-    expect(true).to.equal(true);
+describe('Console plugin template test', () => {
+  before(() => {
+    cy.login();
+
+    if (!isLocalDevEnvironment) {
+      console.log('this is not a local env, installig helm');
+
+      cy.exec('cd ../../console-plugin-template && ./install_helm.sh', {
+        failOnNonZeroExit: false,
+      }).then((result) => {
+        cy.log('Error installing helm binary: ', result.stderr);
+        cy.log(
+          'Successfully installed helm binary in "/tmp" directory: ',
+          result.stdout,
+        );
+
+        installHelmChart('/tmp/helm');
+      });
+    } else {
+      console.log('this is a local env, not installing helm');
+
+      installHelmChart('helm');
+    }
   });
-  it('Does not do much! Should fail', () => {
-    expect(true).to.equal(false);
+
+  afterEach(() => {
+    checkErrors();
+  });
+
+  after(() => {
+    if (!isLocalDevEnvironment) {
+      deleteHelmChart('/tmp/helm');
+    } else {
+      deleteHelmChart('helm');
+    }
+    cy.logout();
+  });
+
+  it('Verify the example page title', () => {
+    cy.get('[data-quickstart-id="qs-nav-home"]').click();
+    cy.get('[data-test="nav"]').contains('Plugin Example').click();
+    cy.url().should('include', '/example');
+    cy.get('[data-test="example-page-title"]').should(
+      'contain',
+      'Hello, Plugin!',
+    );
   });
 });
-
-if (!Cypress.env('OPENSHIFT_CI') || Cypress.env('PLUGIN_TEMPLATE_PULL_SPEC')) {
-  describe('Console plugin template test', () => {
-    before(() => {
-      cy.login();
-
-      if (!isLocalDevEnvironment) {
-        console.log('this is not a local env, installig helm');
-
-        cy.exec('cd ../../console-plugin-template && ./install_helm.sh', {
-          failOnNonZeroExit: false,
-        }).then((result) => {
-          cy.log('Error installing helm binary: ', result.stderr);
-          cy.log(
-            'Successfully installed helm binary in "/tmp" directory: ',
-            result.stdout,
-          );
-
-          installHelmChart('/tmp/helm');
-        });
-      } else {
-        console.log('this is a local env, not installing helm');
-
-        installHelmChart('helm');
-      }
-    });
-
-    afterEach(() => {
-      checkErrors();
-      cy.logout();
-    });
-
-    after(() => {
-      cy.logout();
-      if (!isLocalDevEnvironment) {
-        deleteHelmChart('/tmp/helm');
-      } else {
-        deleteHelmChart('helm');
-      }
-    });
-
-    it('Does not do much! Should pass', () => {
-      expect(true).to.equal(true);
-    });
-    it('Verify the example page title', () => {
-      cy.get('[data-quickstart-id="qs-nav-home"]').click();
-      cy.get('[data-test="nav"]').contains('Plugin Example').click();
-      cy.url().should('include', '/example');
-      cy.get('[data-test="example-page-title"]').should(
-        'contain',
-        'Hello, Plugin!',
-      );
-    });
-    it('Does not do much! Should fail', () => {
-      expect(true).to.equal(false);
-    });
-  });
-}
