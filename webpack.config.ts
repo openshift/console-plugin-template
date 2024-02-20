4c6bd99c9822e5d76c0df3f6a773bd235a514e3f
Howard Gao <howard.gao@gmail.com>
2024-02-20 11:42:18 +0800

    Fix issue #57 Hot reload of plugin is broken in 4.14
    Because the dev server has cache enabled thus when the plugin manifest is
    requested the second time it returns empty content, causing the plugin
    reload to fail. The fix is to disable the cache on the dev server.

--- webpack.config.ts
+++ webpack.config.ts
@@ -65,7 +65,8 @@ const config: Configuration = {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
+      "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization",
+      "Cache-Control": "no-store",
     },
     devMiddleware: {
       writeToDisk: true,
