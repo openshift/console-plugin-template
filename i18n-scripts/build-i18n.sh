f5d284a6594806e36e9d0ca99e52e1fdbdf4a193
logonoff <git@logonoff.co>
2026-07-21 09:19:30 -0400

    chore: migrate i18next-parser to i18next-cli

--- i18n-scripts/build-i18n.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -exuo pipefail
-
-FILE_PATTERN="{!(dist|node_modules)/**/*.{js,jsx,ts,tsx,json},*.{js,jsx,ts,tsx,json}}"
-
-i18next "${FILE_PATTERN}" [-oc] -c "./i18next-parser.config.js" -o "locales/\$LOCALE/\$NAMESPACE.json"
